a57eaa146adf9c5aaf8759a4a21f8faea95ef3bf
nickwill24 <nick2428.nw@gmail.com>
2021-04-12 17:47:42 -0400

    Read token

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -12,5 +12,11 @@ def create_token(payload):
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
 
-def create_token(payload):
-    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+def read_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except jwt.InvalidSignatureError:
+        return "Signature Invalid"
+    except jwt.InvalidTokenError:
+        return "Token Invalid"
