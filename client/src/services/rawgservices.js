bfcd988650a07676a4c9cec7914a53b19feb1937
nickwill24 <nick2428.nw@gmail.com>
2021-04-13 17:04:12 -0400

    Setup API

---
+++ client/src/services/rawgservices.js
@@ -0,0 +1,2 @@
+const API_KEY= process.env.REACT_APP_RAWG_KEY
+
---
+++ client/src/store/reducers/PostReducers.js
@@ -0,0 +1,7 @@
+const { UPDATE_POST, DELETE_POST, CREATE_POST, GET_POSTS, GET_POST} = require('../types')
+
+const iState ={
+    post:''
+}
+
+cons
