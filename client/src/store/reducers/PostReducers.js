a5744a43320405a20133847beb4dbe9c64979870
nickwill24 <nick2428.nw@gmail.com>
2021-04-13 17:17:22 -0400

    Post Reducer done

--- client/src/store/reducers/PostReducers.js
+++ client/src/store/reducers/PostReducers.js
@@ -4,4 +4,20 @@ const iState ={
     post:''
 }
 
-cons
+const PostReducer = (state = iState, action) => {
+    switch (action.type) {
+    case GET_POSTS:
+        return { ...state, post: action.payload }
+    case GET_POST:
+        return{...state, post: action.payload}
+    case UPDATE_POST:
+        return{...state, post: action.payload}
+    case DELETE_POST:
+        return{...state, post: action.payload}
+    case CREATE_POST:
+        return{...state, post: action.payload}
+    default:
+        return { ...state }
+    }
+}
+export default PostReducer
