b72bbedbd4b853c02b4d69f361089353fc786916
nickwill24 <nick2428.nw@gmail.com>
2021-04-13 19:07:53 -0400

    game reducer done

--- client/src/store/actions/PostAction.js
+++ client/src/store/actions/PostAction.js
@@ -1,5 +1,5 @@
 import {CreatePost, UpdatePost, DeletePost, GetPost, GetPosts} from '../../services/PostService'
-import {UPDATE_POST, CREATE_POST, DELETE_POST, GET_POSTS, GET_POST} from '../types'
+import {UPDATE_POST, DELETE_POST, GET_POSTS, GET_POST} from '../types'
 
 
 export const getPosts = () => async (dispatch) => {
@@ -35,7 +35,7 @@ export const deletePost = (id) => async (dispatch) => {
     try {
     const post = await DeletePost(id)
     dispatch({
-        type: REMOVE_IDEA,
+        type: DELETE_POST,
         payload: id
     })
     } catch (error) {
---
+++ client/src/store/reducers/GameReducer.js
@@ -0,0 +1,18 @@
+const {GET_GAME, GET_GAMES}= require('../types')
+
+const iState = {
+    games= [],
+    details=''
+}
+
+const GameReducer = (state = iState, action) => {
+    switch (action.type) {
+    case GET_GAMES:
+        return { ...state, games: action.payload }
+    case GET_GAME:
+        return{...state, games: action.payload}
+    default:
+        return { ...state }
+    }
+}
+export default GameReducer
