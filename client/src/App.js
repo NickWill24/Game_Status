d39f8383b904c208a690f3e797e4c6af34d23714
nickwill24 <nick2428.nw@gmail.com>
2021-04-13 20:56:31 -0400

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome To GameStatus</h1>
+      <Switch>
+
+      </Switch>
     </div>
   );
 }
---
+++ client/src/services/appservice.js
@@ -0,0 +1,5 @@
+import Axios from 'axios'
+
+const Client = Axios.create({ baseURL: 'https://rawg.io/games' })
+
+export default Client
