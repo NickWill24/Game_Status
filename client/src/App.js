f88c0f4f849d8bcbe4861cedf6a909f9e253aaa8
nickwill24 <nick2428.nw@gmail.com>
2021-04-14 12:25:10 -0400

    got Api connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import '../src/styles/App.css'
+import {GetGames} from './services/rawgservices'
 
 function App() {
+useEffect(
+  ()=>GetGames()
+)
   return (
     <div className="App">
       <h1>Welcome To GameStatus</h1>
--- client/src/services/rawgservices.js
+++ client/src/services/rawgservices.js
@@ -1,19 +1,21 @@
-// import Client from "."
+import Client from "."
 
-// const API_KEY= process.env.REACT_APP_RAWG_KEY
+const API_KEY= process.env.REACT_APP_RAWG_KEY
 
-// export const GetGames = async () => {
-// try {
-//     const res 
-// } catch (error) {
-//     throw error
-//     }
-// }
+export const GetGames = async () => {
+try {
+    const res= await Client.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=100`)
+    console.log(res.data)
+} catch (error) {
+    throw error
+    }
+}
 
-// export const GetGame = async (id) => {
-// try {
-//     const res
-// } catch (error) {
-//     throw error
-// }
-// }
+export const GetGame = async (id) => {
+try {
+    const res = await Client.get(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`)
+    console.log(res.data)
+} catch (error) {
+    throw error
+}
+}
