2d0fe5d25373e2d5421f33f4b0aff89fb2b3f1cb
nickwill24 <nick2428.nw@gmail.com>
2021-04-14 12:32:36 -0400

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import '../src/styles/App.css'
 import {GetGames} from './services/rawgservices'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
 useEffect(
@@ -9,6 +10,9 @@ useEffect(
   return (
     <div className="App">
       <h1>Welcome To GameStatus</h1>
+      <Switch>
+        <Route exact path='/' component={Game}/>
+      </Switch>
     </div>
   );
 }
