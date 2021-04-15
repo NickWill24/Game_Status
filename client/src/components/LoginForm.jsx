19d45853747c11e2a6faa2586cf99050db4a3f91
nickwill24 <nick2428.nw@gmail.com>
2021-04-14 22:38:41 -0400

    login component

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const loginForm = (props) =>{
+    
+
+    return (
+        <div>
+            <h1>Login Form</h1>
+
+            <form>
+                <input
+                type='text'
+                name='email'
+                // value={name}
+                placeholder="XXXXXX@email.com"
+                />
+                <input
+                type='text'
+                name="password"
+                // value={name}
+                placeholder='1234'
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default loginForm
